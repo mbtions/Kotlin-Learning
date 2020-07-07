d74cc476c966f01f5d205006ac242155520d3e9a
Meenakshi Bharadwaj <minakshi29bharadwaj@gmail.com>
2020-07-07 21:35:11 +0530

    Added HelloWorld program in kotlin

--- 01_HelloWorld.kt
+++ 01_HelloWorld.kt
@@ -1,5 +1,6 @@
 // Hello World Program
 
+// we use "fun" keyword to define a function, main is the function which gets executes by the compiler , it is a mandatory function
 fun main(args : Array<String>) {
-    println("Hello, World!")
+    println("Hello, World!")   // "println" print the value it has in parenthesis with a line since println means : print line
 }
